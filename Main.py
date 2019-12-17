bd8e0ab948e36f56b616095491a01b03b527e4b9
ArtemNagaev <31846535+ArtemNagaev@users.noreply.github.com>
2019-12-17 20:17:45 +0500

    Update 02
    Распознование машин на парковке

--- Main.py
+++ Main.py
@@ -27,3 +27,39 @@ for line in lines:
     cv2.line(blank_image_parking, (x1, y1), (x2, y2), (255, 255, 255), 2)
 
 cv2.imwrite("test.jpg", blank_image_parking)
+
+# Создание пустого изоброжения для определения машин
+blank_image_cars = np.zeros((1080, 1920, 3), np.uint8)
+
+# Указываем границы HSV
+hsv_min = np.array((0, 45, 5), np.uint8)
+hsv_max = np.array((187, 255, 255), np.uint8)
+
+# меняем цветовую модель с BGR на HSV
+hsv = cv2.cvtColor(imgParkingFull, cv2.COLOR_BGR2HSV)
+# применяем цветовой фильтр
+thresh = cv2.inRange(hsv, hsv_min, hsv_max)
+# Ищем контуры на изображении
+contours0, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+# cv2.imshow("linesDetected", contours0)
+
+boxArr = [];
+# перебираем все найденные контуры в цикле
+for cnt in contours0:
+    # пытаемся вписать прямоугольник
+    rect = cv2.minAreaRect(cnt)
+    # поиск четырех вершин прямоугольника
+    box = cv2.boxPoints(rect)
+    # округление координат
+    box = np.int0(box)
+    # вычисление площади
+    area = int(rect[1][0] * rect[1][1])
+    if area > 10000:
+        cv2.drawContours(blank_image_cars, [box], 0, (255, 255, 255), -1)
+        cv2.drawContours(imgParkingFull, [box], 0, (0, 255, 0), 2)
+        boxArr.append(box)
+
+# Сохранение контуров машин и линий парковки
+# cv2.imwrite("lines.jpg", blank_image_parking)
+cv2.imwrite("cars.jpg", blank_image_cars)
